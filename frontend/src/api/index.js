645a25e208f328591037bea5527bb0f4968cc19d
gtrirf <iravshanov90@gmail.com>
2026-03-11 22:25:01 +0500

    frontend bug resolved

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: '/api/v1/',
+  baseURL: 'https://api.i37.uz/api/v1/',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
